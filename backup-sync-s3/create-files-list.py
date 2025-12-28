6d0789ac3aff428fa7d3345254edb029bdf1e3c0
Daniel <d@stuhrs.dk>
2025-12-28 20:53:17 +0100

    add file count

--- backup-sync-s3/create-files-list.py
+++ backup-sync-s3/create-files-list.py
@@ -26,6 +26,7 @@ def main()-> None:
         print(f"{FILE_LIST} already exists. Remove it first if you want to recreate it.")
         return
 
+    file_count = 0
     with open(FILE_LIST, "w") as f_list:
         for file_path in files_dir.rglob(f'*{FILE_EXTENSION}'):
             print(f"Adding file {file_path}...")
@@ -35,7 +36,9 @@ def main()-> None:
             f_list.write(
                 f"{file_path}{CELL_DELIMITER}{file_created.strftime(DATE_FORMAT)}{CELL_DELIMITER}{digest}\n")
             print(f"{file_path} - OK!")
-    print(f"File list created at {FILE_LIST}")
+            file_count += 1
+
+    print(f"File list created at {FILE_LIST} with {file_count} files.")
 
 
 if __name__ == "__main__":
